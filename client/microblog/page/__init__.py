1dc90e19aed0d841187860305b73b2d49edd93f8
kepner wu <kepner_wu@hotmail.com>
2018-06-18 10:34:59 +0800

    first commit

---
+++ client/microblog/page/__init__.py
@@ -0,0 +1,7 @@
+'''这是个包存储所有的页面'''
+import sys
+
+sys.path.append('/home/tarena/aid1803/microblog/client/microblog/page')
+# sys.path.append('/Users/vicshang/kepner/study/microblog/client/microblog/page')
+
+# sys.path.append('/Users/guolei/Desktop/aid1803/microblog/microblog/msg')
