2618385aaa83f187f1533c4bc0a17149a8e26b78
kep-wu <1317030043@qq.com>
2018-06-20 16:43:10 +0800

    路径修改

--- client/microblog/__init__.py
+++ client/microblog/__init__.py
@@ -2,5 +2,5 @@
 import sys
 # sys.path.append('/Users/guolei/Desktop/aid1803/microblog/microblog')
 
-sys.path.append('/home/tarena/aid1803/microblog/client/microblog/')
+sys.path.append('/home/tarena/microblog/client/microblog/')
 # sys.path.append('/Users/vicshang/kepner/study/microblog/client/microblog/')
--- client/microblog/page/__init__.py
+++ client/microblog/page/__init__.py
@@ -1,7 +1,3 @@
-'''这是个包存储所有的页面'''
-import sys
-
-sys.path.append('/home/tarena/aid1803/microblog/client/microblog/page')
-# sys.path.append('/Users/vicshang/kepner/study/microblog/client/microblog/page')
-
-# sys.path.append('/Users/guolei/Desktop/aid1803/microblog/microblog/msg')
+'''
+这是个包存储所有的页面
+'''
